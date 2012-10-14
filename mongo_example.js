41521de564d6f4bd931e6111cdadd0373e146f65
Paul Booth <thephantompaulbooth@gmail.com>
2012-10-14 14:20:58 -0400

    push to server

--- mongo_example.js
+++ mongo_example.js
@@ -1,3 +1,6 @@
+var mongo = require('mongodb'),
+  Db = mongo.Db;
+
 var client = new Db('test', new Server("127.0.0.1", 27017, {})),
         test = function (err, collection) {
           collection.insert({a:2}, function(err, docs) {
