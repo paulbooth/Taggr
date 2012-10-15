2032dadb0efd0b890950f81db85b16504c2969fb
Paul Booth <paul@goban.(none)>
2012-10-15 23:50:48 +0000

    Made a splash page, removed doortracker file

--- public/doortracker-client.js
+++
@@ -1,3 +0,0 @@
-curl -F 'access_token=AAACTnrKaW1kBALX0DqEp5DaZA9GZA0dYN2kqwZAkH3shnLYLIKorifE1ZARfZCRf8Aux8YedmmSX4tmzm6CI6amZCheOuVDgvtj0IKJoOZCP8b8AiekeZAfO' \
-     -F 'spot=http://thepaulbooth.com:3727/spot/SCOPE room' \
-        'https://graph.facebook.com/me/fbtaggr:tag'
